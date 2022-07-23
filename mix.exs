e26c9eb80e61e8a45b6883d033b91bcc4577c24b
Leonard Dauksza <leonard@dauksza.com>
2022-07-23 12:19:56 -0700

    add ecto_shorts, absynthe and credo deps

--- mix.exs
+++ mix.exs
@@ -41,7 +41,16 @@ defmodule PirateXchange.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+
+      {:absinthe, "~> 1.6"},
+      {:absinthe_plug, "~> 1.5"},
+      {:absinthe_phoenix, "~>2.0"},
+      {:dataloader, "~> 1.0"},
+
+      {:ecto_shorts, "~> 2.0"},
+
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
     ]
   end
 
