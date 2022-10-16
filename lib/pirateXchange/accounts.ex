874c0e0edc9baf0e4d756bc83404f55c0e1541ab
Leonard Dauksza <leonard@dauksza.com>
2022-10-16 12:16:44 -0700

    add all_users function and tests

--- lib/pirateXchange/accounts.ex
+++ lib/pirateXchange/accounts.ex
@@ -4,4 +4,7 @@ defmodule PirateXchange.Accounts do
 
   @spec create_user(map) :: {:ok, User.t()} | {:error, String.t()}
   def create_user(params), do: Actions.create(User, params)
+
+  @spec all_users() :: [User.t()]
+  def all_users(), do: Actions.all(User)
 end
--- test/pirateXchange/accounts_test.exs
+++ test/pirateXchange/accounts_test.exs
@@ -7,17 +7,18 @@ defmodule PirateXchange.AccountsTest do
   }
 
   @valid_user_params %{name: "Jack Sparrow", email: "sparrow@theblackperl.com"}
+  @valid_user2_params %{name: "Blackbeard", email: "unshaven@queensrevenge.com"}
   @invalid_user_params %{name: nil, email: nil}
 
   describe "create_user/1" do
-    test "creates a user with valid params" do
+    test "should create a user with valid params" do
       assert {:ok, %User{name: "Jack Sparrow", email: "sparrow@theblackperl.com"}} =
         Accounts.create_user(@valid_user_params)
 
       [%User{}] = Repo.all(User)
     end
 
-    test "does not create user with duplicate email" do
+    test "should not create user with duplicate email" do
       assert {:ok, %User{name: "Jack Sparrow", email: "sparrow@theblackperl.com"}} =
         Accounts.create_user(@valid_user_params)
 
@@ -27,7 +28,7 @@ defmodule PirateXchange.AccountsTest do
       assert %{email: ["has already been taken"]} == errors_on(changeset)
     end
 
-    test "does not create user with invalid params" do
+    test "should not create user with invalid params" do
       assert {:error, %Ecto.Changeset{} = changeset} =
         Accounts.create_user(@invalid_user_params)
 
@@ -35,4 +36,17 @@ defmodule PirateXchange.AccountsTest do
     end
   end
 
+  describe "all_users/0" do
+    setup [:setup_users]
+
+    test "should return all users", %{users: users} do
+      assert users = Accounts.all_users()
+    end
+  end
+
+  defp setup_users(_ctx) do
+    Accounts.create_user(@valid_user_params)
+    Accounts.create_user(@valid_user2_params)
+    %{users: Repo.all(User)}
+  end
 end
